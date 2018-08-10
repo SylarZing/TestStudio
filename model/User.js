9b835c23a312136b130fc07ba9fb82d3b561fa88
SylarZing <344847204@qq.com>
2018-08-10 20:55:14 +0800

    init Project

---
+++ model/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(){
+        this.strUserName = '';
+        this.strPhone = null;
+        this.strEmail = null;
+        this.strPassword = '';
+        this.iUserType = 5;
+    }
+}
+
+module.exports = User;
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var User = require('../model/User');
+
+
+router.post('/login',function(req, res, next){
+    console.log(req.body);
+    console.log(req.body.username);
+    console.log(req.body.password);
+
+    res.send('login successfully.');
+});
+
+router.post('/register', function(req, res, next){
+
+    var user = new User();
+    user.strUserName = req.body.UserName;
+    user.strPhone = req.body.Phone;
+    user.strEmail = req.body.Email;
+    user.strPassword = req.body.Password;
+    user.iUserType = req.body.UserType;
+    
+    res.send('sign up successfully.');
+});
+
+module.exports = router;
