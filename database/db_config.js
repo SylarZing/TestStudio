b9707edde1aaefc4265205370975f64af7c89f5d
SylarZing <344847204@qq.com>
2018-08-12 11:04:38 +0800

    import mysql & add db_config.js

---
+++ database/db_config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    mysql : {
+        host:'127.0.0.1',
+        user:'root',
+        password:'12345678',
+        port:'3306',
+        database:'PENGERSTUDIO'
+    }
+};
